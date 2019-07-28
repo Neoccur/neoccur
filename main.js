f86d2848cbf1ace9ced960f73ea4ed0fbd3d3960
Oxyde2 <maxime.djomby@gmail.com>
2019-07-28 09:28:43 +0200

    **Added** Icon & Nav bar scroll reaction :tada:

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
 // You may call these functions in your HTML anytime
 // As long as this file is loaded before-hand
 
+function underConstruction() {
+  alert("This website is currently under construction and is not fully operational. You have been warned.");
+};
+
 function languageClickEvent(lang) {
   switch (lang) {
     case "en":
@@ -13,6 +17,18 @@ function languageClickEvent(lang) {
 
 };
 
-function underConstruction() {
-  alert("This website is currently under construction and is not fully operational. You have been warned.");
+function navBarReact() {
+  console.log("scrolled" + document.documentElement.scrollTop);
+  let myNav = document.getElementById('myNav');
+
+    if (document.documentElement.scrollTop > 500) {
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
+    }
+
+    else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+    };
+
 };
