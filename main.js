7935b3b89e9d481984a3d1d1c1826ddfff0921ae
Oxyde2 <maxime.djomby@gmail.com>
2019-07-30 09:21:47 +0200

    Refactored css :gear:, removed trackers :bomb:

--- main.js
+++ main.js
@@ -2,22 +2,6 @@
 // You may call these functions in your HTML anytime
 // As long as this file is loaded before-hand
 
-function resolution() {
-  let res = document.getElementById('res');
-  res.innerHTML = "Resolution: " + window.innerWidth + "x" + window.innerHeight;
-};
-
-function mousePos() {
-  let mouseX = event.clientX;
-  let mouseY = event.clientY;
-  mouse.innerHTML = "Mouse x: " + mouseX + "  y: " + mouseY;
-};
-
-function underConstruction() {
-  alert("This website is currently under construction and is not fully operational. You have been warned.");
-  resolution();
-};
-
 function languageClickEvent(lang) {
   if (lang == "en") {
       document.location.href = "http://neoccur.com/";
@@ -32,13 +16,13 @@ function navBarReact() {
   let myNav = document.getElementById('myNav');
 
     if (document.documentElement.scrollTop > 300) {
-        myNav.classList.add("nav-normal");
-        myNav.classList.remove("nav-scroll");
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
     }
 
     else {
-        myNav.classList.add("nav-scroll");
-        myNav.classList.remove("nav-normal");
+      myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
     };
 
 };
